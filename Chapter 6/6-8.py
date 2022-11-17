8fdac527b2731aa3b76bcc9a7502fb214f45fdd2
Ruslan <ruslanmorozov357@gmail.com>
2022-11-17 21:23:10 +0200

    add ch 7

--- Chapter 6/6-8.py
+++
@@ -1,8 +0,0 @@
-pet_1 = {'cat': 'jen'}
-pet_2 = {'dog': 'phil'}
-pet_3 = {'parrot': 'sarah'}
-pets = [pet_1, pet_2, pet_3]
-
-for pet in pets:
-    for k, v in pet.items():
-        print(f'{v.title()} has a {k.title()}')
--- Chapter 6/6-9.py
+++
@@ -1,8 +0,0 @@
-favorite_places = {'dima': ['dolomites', 'yellowstone', 'milford sound'],
-                   'elena': ['red center', 'bruges'],
-                   'max': ['tromso', 'loire valley', 'santorini']}
-for name, place in favorite_places.items():
-    print(f"{name.title()}'s favorite places:")
-    for pl in place:
-        print(f'{pl.title()}')
-    print()
